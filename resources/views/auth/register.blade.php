8c414065389103aa484070531c6961a998f7eeb4
Vahn Marty <vahnmarty@gmail.com>
2021-03-15 01:21:01 +0800

    register: create user

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,31 +6,72 @@
 
         <x-jet-validation-errors class="mb-4" />
 
-        <form method="POST" action="{{ route('register') }}">
+        <form method="POST"
+              action="{{ route('register') }}">
             @csrf
 
             <div>
-                <x-jet-label for="name" value="{{ __('Name') }}" />
-                <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                <x-jet-label for="First Name"
+                             value="{{ __('First Name') }}" />
+                <x-jet-input id="first_name"
+                             class="block mt-1 w-full"
+                             type="text"
+                             name="first_name"
+                             :value="old('first_name')"
+                             required
+                             autofocus
+                             autocomplete="first_name" />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                <x-jet-label for="last_name"
+                             value="{{ __('Last Name') }}" />
+                <x-jet-input id="last_name"
+                             class="block mt-1 w-full"
+                             type="text"
+                             name="last_name"
+                             :value="old('last_name')"
+                             required
+                             autofocus
+                             autocomplete="last_name" />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+                <x-jet-label for="email"
+                             value="{{ __('Email') }}" />
+                <x-jet-input id="email"
+                             class="block mt-1 w-full"
+                             type="email"
+                             name="email"
+                             :value="old('email')"
+                             required />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+                <x-jet-label for="password"
+                             value="{{ __('Password') }}" />
+                <x-jet-input id="password"
+                             class="block mt-1 w-full"
+                             type="password"
+                             name="password"
+                             required
+                             autocomplete="new-password" />
+            </div>
+
+            <div class="mt-4">
+                <x-jet-label for="password_confirmation"
+                             value="{{ __('Confirm Password') }}" />
+                <x-jet-input id="password_confirmation"
+                             class="block mt-1 w-full"
+                             type="password"
+                             name="password_confirmation"
+                             required
+                             autocomplete="new-password" />
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900"
+                   href="{{ route('login') }}">
                     {{ __('Already registered?') }}
                 </a>
 
