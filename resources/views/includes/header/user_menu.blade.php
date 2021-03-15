4fe257f0d961082f7348252986a5bcd2293eaab5
Vahn Marty <vahnmarty@gmail.com>
2021-03-16 04:56:33 +0800

    profile

--- resources/views/includes/header/user_menu.blade.php
+++ resources/views/includes/header/user_menu.blade.php
@@ -37,7 +37,7 @@
          role="menu"
          aria-orientation="vertical"
          aria-labelledby="user-menu">
-        <a href="#"
+        <a href="{{ url('user/profile') }}"
            class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
            role="menuitem">Your Profile</a>
         <a href="#"
